a047095e6d91168fead0c7d7068d700a29887eb3
rahul <kavirahul085@gmail.com>
2020-04-03 07:38:31 -0700

    modified code to display different print styles based on value type

--- c02-The-Basics/c02s03/values.go
+++ c02-The-Basics/c02s03/values.go
@@ -1,18 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, world")
-	fmt.Println(6)
-	fmt.Println("The value 6:", 6)
-	fmt.Println("3 + 1 =", 3+1)
-	fmt.Println("2016 divided by 9 divided by 15 =", 2016/9/15)
-	fmt.Println("9 divided by 15 = ", 9/15)
-	fmt.Println("9.0 divided by 15 =", 9.0/15)
-	fmt.Println("The value of Pi =", 3.141592)
-	fmt.Println("The statement 'pigs can fly' is", false)
-	fmt.Println("My comlex number is", 11+4i)
-	fmt.Println("An slice of 4 integers:", []int{1, 2, 3, 5})
-	fmt.Println("A 3 element array of string, with only 2 elements initialized:", [3]string{"hello", "world"})
+	fmt.Println("this is so cool")
+	fmt.Println("6 + 3.0", 6+3.0)
+	fmt.Println("slice of integers", [2]int{3, 4})
+	fmt.Println("is true == false?", true == false)
+	fmt.Printf("this is so cool %f %s", 3/4.0, "yes\n")
+	fmt.Printf("so cool %f", 3/4.4)
 }
